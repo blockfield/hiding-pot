163e376fce57bdecea71d479daae964bf2b9a23d
Drygin <DryginAlexander@gmail.com>
2021-11-07 18:04:52 +0300

    add deploy scripts

---
+++ contract/scripts/deploy.js
@@ -0,0 +1,32 @@
+async function main() {
+
+    const [deployer] = await ethers.getSigners();
+    
+    console.log(
+        "Deploying contracts with the account:",
+        deployer.address
+    );
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const Hasher = await ethers.getContractFactory("Hasher");
+    const hasher = await Hasher.deploy();
+
+    console.log("Hasher address:", hasher.address);
+
+    const Verifier = await ethers.getContractFactory("Verifier");
+    const verifier = await Verifier.deploy();
+
+    console.log("Verifier address:", verifier.address);
+
+    const HidingPot = await ethers.getContractFactory("HidingPot");
+    const hidingPot = await HidingPot.deploy(verifier.address, hasher.address, 10*10**9, 10); // 10 CLV, 10 tree levels
+    
+    console.log("HidingPot address:", hidingPot.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
