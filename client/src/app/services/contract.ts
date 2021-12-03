07cf8b46838879634bcda0e7d7044612278ba394
egor.medvedev <e.medvedev@city-mobil.ru>
2021-12-03 17:24:41 +0300

    Withdraw money with clover and likelib

--- client/src/app/services/contract.ts
+++ client/src/app/services/contract.ts
@@ -5,8 +5,6 @@ import { abiContract } from "../api/abi-contract";
 import { Proof } from "../models/proof";
 
 export class Contract {
-    static balance = 1000
-
     private lk: LikeLib
     private account: LikeLib.Account
     private myPublicKey: string
@@ -27,17 +25,14 @@ export class Contract {
         return new Promise((resolve) => {
             console.log('CALLING method `deposit`', commitment, amount)
 
-            Contract.balance = Contract.balance - 10
+            this.contract.deposit(commitment, amount * 10**9, 500000, function(err: any, info: any) {
+                console.log('RESULT in method `deposit`', err, info)
+                if (err) {
+                    console.log('ERROR in method `deposit`', err)
+                }
 
-            resolve()
-            // this.contract.deposit(commitment, amount * 10**9, 500000, function(err: any, info: any) {
-            //     console.log('RESULT in method `deposit`', err, info)
-            //     if (err) {
-            //         console.log('ERROR in method `deposit`', err)
-            //     }
-
-            //     resolve()
-            // })
+                resolve()
+            })
         })
     }
 
@@ -45,22 +40,14 @@ export class Contract {
         return new Promise((resolve) => {
             console.log('CALLING method `withdraw`', proof, root, nullifierHash)
 
-            let j = 0
-            for (let i = 0; i < 1000000000; i++) {
-                j++
-            }
+            this.contract.withdraw(proof.a, proof.b, proof.c, [root, nullifierHash], 0, 500000, function(err: any, info: any) {
+                console.log('RESULT in method `withdraw`', err, info)
+                if (err) {
+                    console.log('ERROR in method `withdraw`', err)
+                }
 
-            Contract.balance = Contract.balance + 10
-
-            resolve()
-            // this.contract.withdraw(proof.a, proof.b, proof.c, [root, nullifierHash], 0, 500000, function(err: any, info: any) {
-            //     console.log('RESULT in method `withdraw`', err, info)
-            //     if (err) {
-            //         console.log('ERROR in method `withdraw`', err)
-            //     }
-
-            //     resolve()
-            // })
+                resolve()
+            })
         })
     }
 
@@ -68,14 +55,13 @@ export class Contract {
         return new Promise((resolve) => {
             console.log('CALLING method `accountInfo`')
 
-            resolve(Contract.balance)
-            // this.lk.getAccountInfo(this.myPublicKey, function(err: any, info: any) {
-            //     if (err) {
-            //         console.log('ERROR in method `getMyBalance`', err)
-            //     }
+            this.lk.getAccountInfo(this.myPublicKey, function(err: any, info: any) {
+                if (err) {
+                    console.log('ERROR in method `getMyBalance`', err)
+                }
 
-            //     resolve(+info.balance)
-            // })
+                resolve(+info.balance)
+            })
         })
     }
 
@@ -83,15 +69,14 @@ export class Contract {
         return new Promise((resolve) => {
             console.log('CALLING method `getLastRoot`')
 
-            resolve("0x101739eff77bcb95d4af1de11c07e2462efac778c017891a7e1e468cdce8e0e3")
-            // this.contract.getLastRoot(0, 500000, function(err: any, info: any) {
-            //     console.log('RESULT in method `getLastRoot`', err, info)
-            //     if (err) {
-            //         console.log('ERROR in method `getLastRoot`', err)
-            //     }
+            this.contract.getLastRoot(0, 500000, function(err: any, info: any) {
+                console.log('RESULT in method `getLastRoot`', err, info)
+                if (err) {
+                    console.log('ERROR in method `getLastRoot`', err)
+                }
 
-            //     resolve(info[0])
-            // })
+                resolve(info[0])
+            })
         })
     }
 
@@ -99,15 +84,14 @@ export class Contract {
         return new Promise((resolve) => {
             console.log('CALLING method `filledSubtrees`', index)
 
-            resolve("0x101739eff77bcb95d4af1de11c07e2462efac778c017891a7e1e468cdce8e0e3")
-            // this.contract.filledSubtrees(index, 0, 500000, function(err: any, info: any) {
-            //     console.log('RESULT in method `filledSubtrees`', err, info)
-            //     if (err) {
-            //         console.log('ERROR in method `filledSubtrees`', err)
-            //     }
+            this.contract.filledSubtrees(index, 0, 500000, function(err: any, info: any) {
+                console.log('RESULT in method `filledSubtrees`', err, info)
+                if (err) {
+                    console.log('ERROR in method `filledSubtrees`', err)
+                }
 
-            //     resolve(info[0].substring(2))
-            // })
+                resolve(info[0].substring(2))
+            })
         })
     }
 
@@ -115,15 +99,14 @@ export class Contract {
         return new Promise((resolve) => {
             console.log('CALLING method `nextIndex`')
             
-            resolve(0)
-            // this.contract.nextIndex(0, 500000, function(err: any, info: any) {
-            //     console.log('RESULT in method `nextIndex`', err, info)
-            //     if (err) {
-            //         console.log('ERROR in method `nextIndex`', err)
-            //     }
+            this.contract.nextIndex(0, 500000, function(err: any, info: any) {
+                console.log('RESULT in method `nextIndex`', err, info)
+                if (err) {
+                    console.log('ERROR in method `nextIndex`', err)
+                }
 
-            //     resolve(info[0])
-            // })
+                resolve(info[0])
+            })
         })
     }
 }
